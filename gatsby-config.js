3dc7b69129acc8e737319450736ab3f92312c5a1
Jimmy <jimmy.beldone@gmail.com>
2020-06-23 17:32:15 +0200

    manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Medikiosk`,
+    description: `Medikiosk`,
+    author: `Medikiosk`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,11 +18,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Medikiosk`,
+        short_name: `Medikiosk`,
         start_url: `/?source=pwa`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#f4e7df`,
+        theme_color: `#265c4f`,
         display: `fullscreen`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
